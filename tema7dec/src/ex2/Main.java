680c902a86e8a13bdbd61457ac9004c4dbfdbf93
elenaman <nicoletaman97@gmail.com>
2025-01-05 15:25:23 +0200

    ex2

---
+++ tema7dec/src/ex2/Main.java
@@ -0,0 +1,9 @@
+package ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, Integer> newStudent = new Pair<>("Elena", 1);
+        System.out.println(newStudent.getKey());
+        System.out.println(newStudent.getValue());
+    }
+}
